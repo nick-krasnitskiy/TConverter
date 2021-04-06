94d47bd57f11d7a670f08f723303c3b90afcc704
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-06 10:01:59 +0300

    Logic was impelemented.

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
 
     @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        let temperatuteCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatuteCelsius)ºC"
+        
+        let tempetaruteFahrenheit = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(tempetaruteFahrenheit)ºF"
     }
     
 }
